db6662e8b818982b1a52239a84df056638b94778
Anna-Sergeevna <4042808@gmail.com>
2021-11-12 14:17:13 +0200

    Добавляет "Телефонную книгу"

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import Container from 'components/Container';
+import Section from 'components/Section';
+import ContactForm from 'components/ContactForm';
+import Filter from 'components/Filter';
+import ContactList from 'components/ContactList';
+import './App.css';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+
+    contacts.map(contact => contact.name).includes(name)
+      ? alert(`${name} is already in contacts.`)
+      : this.setState(prevState => ({
+          contacts: [contact, ...prevState.contacts],
+        }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter),
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const addContact = this.addContact;
+    const changeFilter = this.changeFilter;
+    const visibleContacts = this.getVisibleContacts();
+    const onDeleteContact = this.deleteContact;
+
+    return (
+      <Container title='Телефонная книга'>
+        <Section title='Phonebook'>
+          <ContactForm onSubmit={addContact} />
+        </Section>
+        <Section title='Contacts'>
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={onDeleteContact}
+          />
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
